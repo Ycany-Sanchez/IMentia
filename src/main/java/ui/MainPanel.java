b4426c0e5a98276ad411966a9abeac62f386a8f2
LAB12_USER ON WS22 <L12Y08W22@LabHCI.net>
2025-11-27 08:20:19 +0800

    new ui

---
+++ src/main/java/ui/MainPanel.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.*;
+
+public class MainPanel {
+    private JPanel panel1;
+    private JButton CAPTUREPHOTOButton;
+    private JButton VIEWCONTACTSButton;
+    private JButton TUTORIALButton;
+    private JPanel CameraPanel;
+    private JPanel ContactsPanel;
+}
