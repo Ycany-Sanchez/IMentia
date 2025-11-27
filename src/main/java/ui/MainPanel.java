9703a94ab05e7062043e6a84e0ff51610f12acb0
LAB12_USER ON WS22 <L12Y08W22@LabHCI.net>
2025-11-27 10:02:08 +0800

    new ui changes

--- src/main/java/ui/MainPanel.java
+++ src/main/java/ui/MainPanel.java
@@ -1,12 +1,85 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPanel {
-    private JPanel panel1;
+    private JPanel mainPanel;
     private JButton CAPTUREPHOTOButton;
     private JButton VIEWCONTACTSButton;
     private JButton TUTORIALButton;
     private JPanel CameraPanel;
     private JPanel ContactsPanel;
+    private JPanel TutorialPanel;
+    private JPanel PersonForm;
+    private JPanel RecognizedForm;
+    private JButton EditContactButton;
+    private JButton deleteButton;
+    private JPanel DisplayPanel;
+    private JPanel ButtonPanel;
+    private JPanel PersonPanel;
+    private JButton BACKTOCAMERAButton;
+
+    private CardLayout cardLayout = new CardLayout();
+
+    private boolean isEditing = false;
+
+    public MainPanel() {
+        DisplayPanel.setLayout(cardLayout);
+        DisplayPanel.add(ContactsPanel, "1");
+        DisplayPanel.add(CameraPanel, "2");
+
+        cardLayout.show(DisplayPanel, "2");
+
+        VIEWCONTACTSButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(DisplayPanel, "1");
+                BACKTOCAMERAButton.setVisible(true);
+                CAPTUREPHOTOButton.setVisible(false);
+                TUTORIALButton.setVisible(false);
+                VIEWCONTACTSButton.setVisible(false);
+            }
+        });
+
+
+        EditContactButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isEditing = !isEditing;
+
+                for (Component c : PersonPanel.getComponents())
+                {
+                    if (c instanceof JPanel itemPanel) {
+                        for (Component inner : itemPanel.getComponents()) {
+                            if (inner instanceof JButton deleteButton) {
+                                deleteButton.setVisible(isEditing);
+                            }
+                            if(inner instanceof JLabel label)
+                            {
+                                label.setHorizontalAlignment(SwingConstants.LEFT);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        BACKTOCAMERAButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(DisplayPanel, "2");
+                BACKTOCAMERAButton.setVisible(false);
+                CAPTUREPHOTOButton.setVisible(true);
+                TUTORIALButton.setVisible(true);
+                VIEWCONTACTSButton.setVisible(true);
+            }
+        });
+    }
+
+    public JPanel getPanel(){
+        return mainPanel;
+    }
+
 }
